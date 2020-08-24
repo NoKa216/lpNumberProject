7e101ae5496b00fbb13f499ad6814561f7fc66ae
NoKa216 <nof1997@gmail.com>
2020-08-24 18:41:19 +0300

    add

--- src/components/Middle/Middle.js
+++ src/components/Middle/Middle.js
@@ -10,7 +10,7 @@ function Middle() {
     const [tav, setTav] = useState("");
     const [isLoading,setLoading] = useState(false);
 
-    const numberHandelerChange = (event) =>{
+    const numberHandleChange = (event) =>{
         if(event.target.value.length <=8){
             setLpNumber(event.target.value);
         }else{
@@ -23,37 +23,25 @@ function Middle() {
         event.preventDefault();
         setLoading(true);
         if(lpNumber){
-            axios.get("https://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&q="+lpNumber)
+            axios.get("https://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&q="+lpNumber) /// מוצא את פרטי הרכב במאגר
                 .then((res)=>{
                     setCarData(res.data.result.records[0])
-                    axios.get("https://data.gov.il/api/3/action/datastore_search?resource_id=c8b9f9c8-4612-4068-934f-d4acd2e3c06e&q="+lpNumber)
+                    axios.get("https://data.gov.il/api/3/action/datastore_search?resource_id=c8b9f9c8-4612-4068-934f-d4acd2e3c06e&q="+lpNumber) /// בודק אם לרכב יש תג נכה
                         .then((res)=>{
                             setTav(res.data.result.records[0]);
                             setLoading(false)
                         }).catch((err)=>{
                             console.log(err);
-                        })
+                        });
                 })
-                .catch(err => {console.log(err);})
+                .catch((err) => {console.log(err);});
+        }else{
+            alert("נא להזין מספר רכב");
         }
     }
-    const loading = isLoading && <div className={classes.Loader}>טוען...</div>;
-    return (
-        <section className={classes.Middle}>
-        <div className={classes.Text}>
-            <h1>חיפוש פרטי רכב לפי מספר רישוי</h1>
-            <p>מאגר זה מכיל מידע רשמי של מספרי רישוי של כלי הרכב הפעילים הפרטיים משנת יצור 
-            1996 ומעלה ומספרי הרישוי של כלי הרכב הפעילים המסחריים במשקל עד 3,500 ק"ג משנת יצור 1998 ומעלה.</p>
-        </div>
-            <img src={plate} className={classes.Plate} alt="lp" />
-        <form onSubmit={chackLpNumberHandeler}>
-        <div className={classes.Lp}>
-            <input type="number" name="lpNumber" pattern="[0-9]*"  className={classes.Input} value={lpNumber} onChange={numberHandelerChange} />
-            <button type="submit" value="submit" className={classes.Btn} disabled={!lpNumber}>חפש!</button>
-        </div>
-        {loading}
-        </form>
-        {carData &&<div className={classes.Data}>
+
+    const data = carData && (
+        <div className={classes.Data}>
             <h2>פרטי הרכב:</h2>
             {tav && <h3>יש לרכב תג נכה</h3>}
             <p>מספר רכב: {carData.mispar_rechev}</p>
@@ -62,7 +50,7 @@ function Middle() {
             <p>רמת גימור:  {carData.ramat_gimur}</p>
             <p>שנת יצור:   {carData.shnat_yitzur}</p>
             <p>תוקף רישיון רכב:  {carData.tokef_dt}</p>
-            <p>תאריך טסט  {carData.mivchan_acharon_dt}</p>
+            <p>תאריך טסט:  {carData.mivchan_acharon_dt}</p>
             <p>בעלות:  {carData.baalut}</p>
             <p>קבוצת זיהום:  {carData.kvutzat_zihum}</p>
             <p>רמת איבזור בטיחותי:  {carData.ramat_eivzur_betihuty}</p>
@@ -71,7 +59,25 @@ function Middle() {
             <p>סוג דגם:  {carData.sug_degem}</p>
             <p>שם דגם:  {carData.degem_cd}</p>
             <p>דגם מנוע:  {carData.degem_manoa}</p>
-        </div>}
+        </div>
+    );
+    const loadingOrData = isLoading ? <div className={classes.Loader}>טוען...</div> : data;
+    return (
+        <section className={classes.Middle}>
+            <div className={classes.Text}>
+                <h1>חיפוש פרטי רכב לפי מספר רישוי</h1>
+                <p>מאגר זה מכיל מידע רשמי של מספרי רישוי של כלי הרכב הפעילים הפרטיים משנת יצור 
+                1996 ומעלה ומספרי הרישוי של כלי הרכב הפעילים המסחריים במשקל עד 3,500 ק"ג משנת יצור 1998 ומעלה.</p>
+            </div>
+            <img src={plate} className={classes.Plate} alt="lp" />
+            <form onSubmit={chackLpNumberHandeler}>
+                <div className={classes.Lp}>
+                    <input type="number" name="lpNumber" pattern="[0-9]*"  className={classes.Input} value={lpNumber} onChange={numberHandleChange} />
+                    <button type="submit" value="submit" className={classes.Btn} disabled={!lpNumber}>חפש!</button>
+                </div>
+                
+            </form>
+            {loadingOrData}
         </section>
     );
 };
